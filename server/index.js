bdbdd665c1427da863c41ae361899af4127069ae
jwklong <jwklong@gmx.com>
2025-07-04 08:00:58 +0100

    stuffs

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import http from 'http'
 
-let data = {
-  players: {
+const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('')
 
-  }
+let data = {
+  players: []
 }
 
 const server = http.createServer((req, res) => {
@@ -19,9 +19,23 @@ const server = http.createServer((req, res) => {
 
         switch (params.op) {
           case "GetPlayerKey": {
+            if (!params.hwkey || !params.name) {
+              res.statusCode = 400
+              res.end("Missing paramaters")
+            }
+
+            let player = data.players.find(v => v.cKey == params.hwkey)
+            if (!player) {
+              player = {
+                cKey: params.hwkey,
+                sKey: genRanHex(32)
+              }
+              data.players.push(player)
+            }
+
             res.statusCode = 200
             res.setHeader('Content-Type', 'application/json')
-            res.end(JSON.stringify({key: params.hwkey}))
+            res.end(JSON.stringify({key: player.sKey}))
             break
           }
           default: {
