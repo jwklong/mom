388c28fd1e2a65196072a69aeb0bb3a7c5b3dc5f
jwklong <jwklong@gmx.com>
2025-07-05 20:18:35 +0100

    more capping

--- server/index.js
+++ server/index.js
@@ -103,7 +103,7 @@ const server = http.createServer((req, res) => {
 
         player.wogc.ballCount = Math.max(Math.min(Number(params.ballCount), 300), 0)
         player.wogc.ballCountAttached = Math.max(Math.min(Number(params.ballCountAttached), 300), 0)
-        player.wogc.height = Math.max(Number(params.height), 0)
+        player.wogc.height = Math.max(Math.min(Number(params.height), 100), 0)
         saveData()
 
         res.statusCode = 200
